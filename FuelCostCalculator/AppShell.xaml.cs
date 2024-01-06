80478c0b47fbb4507acfefaf76bcac5a49ed9aa5
Miika Rinta-Korkeamäki <e1900516@edu.vamk.fi>
2024-01-06 12:35:15 +0200

    Add project files.

---
+++ FuelCostCalculator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FuelCostCalculator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FuelCostCalculator/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace FuelCostCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        double fuelCostEuros;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitBtn_Clicked(object sender, EventArgs e)
+        {
+            double distance = Convert.ToDouble(TravelledDistance.Text);
+            double avgFuelConsumption = Convert.ToDouble(AvgFuelConsumption.Text);
+            double gasPrice = Convert.ToDouble(GasPrice.Text);
+
+            fuelCostEuros = (distance / 100) * avgFuelConsumption * gasPrice;
+            FuelCostEur.Text = Convert.ToString(fuelCostEuros);
+        }
+    }
+
+}
