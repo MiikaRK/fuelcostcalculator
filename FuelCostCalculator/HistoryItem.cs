7cd67069866527328208303b1ce9788ee924718f
Miika Rinta-Korkeamäki <e1900516@edu.vamk.fi>
2024-04-01 12:43:42 +0300

    Implemented history feature

---
+++ FuelCostCalculator/HistoryItem.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+
+namespace FuelCostCalculator
+{
+    public class HistoryItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        public DateTime Date { get; set; }
+        public double Distance { get; set; }
+        public double AvgFuelConsumption { get; set; }
+        public double GasPrice { get; set; }
+        public int NumberOfPeople { get; set; }
+        public double SharedCost { get; set; }
+
+        public HistoryItem()
+        {
+            Date = DateTime.Now;
+        }
+    }
+}
---
+++ FuelCostCalculator/HistoryItemViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelCostCalculator
+{
+    public class HistoryItemViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public double Distance { get; set; }
+        public double AvgFuelConsumption { get; set; }
+        public double GasPrice { get; set; }
+        public int NumberOfPeople { get; set; }
+        public double Cost { get; set; }
+
+        public HistoryItemViewModel(HistoryItem item)
+        {
+            ID = item.ID;
+            Date = item.Date;
+            Distance = item.Distance;
+            AvgFuelConsumption = item.AvgFuelConsumption;
+            GasPrice = item.GasPrice;
+            NumberOfPeople = item.NumberOfPeople;
+            Cost = Math.Round(item.SharedCost, 2);
+        }
+    }
+}
